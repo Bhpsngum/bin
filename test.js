d4310ff7cddf591863e1980d071d0f099a362bd6
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-05-08 14:42:31 +0700

    Update test.js

--- test.js
+++ test.js
@@ -151,7 +151,7 @@ Object.assign(Alien.prototype, AlienExtender);
 
 game.modding.tick = function(t) {
     var e;
-    if (this.game.tick(t), e = Date.now(),this.updateShips(), this.checkForTimers(), this.checkForIntervals(), null != this.context.tick && this.context.tick(this.game), e = Date.now() - e, this.max_tick_time = Math.max(this.max_tick_time, e), this.tick_time += e, this.tick_count += 1, this.tick_count >= 600) return this.terminal.echo("Tick CPU time: average " + Math.round(this.tick_time / this.tick_count) + " ms ; max " + Math.round(this.max_tick_time) + " ms"), this.terminal.echo("Data sent: " + Math.round(this.I1I0I.log_sent / this.tick_count * 60) + " bytes per second"), this.tick_count = 0, this.tick_time = 0, this.max_tick_time = 0, this.I1I0I.log_sent = 0
+    if (this.game.tick(t), e = Date.now(),this.game.updateShips(), this.game.checkForTimers(), this.game.checkForIntervals(), null != this.context.tick && this.context.tick(this.game), e = Date.now() - e, this.max_tick_time = Math.max(this.max_tick_time, e), this.tick_time += e, this.tick_count += 1, this.tick_count >= 600) return this.terminal.echo("Tick CPU time: average " + Math.round(this.tick_time / this.tick_count) + " ms ; max " + Math.round(this.max_tick_time) + " ms"), this.terminal.echo("Data sent: " + Math.round(this.I1I0I.log_sent / this.tick_count * 60) + " bytes per second"), this.tick_count = 0, this.tick_time = 0, this.max_tick_time = 0, this.I1I0I.log_sent = 0
 }
 game.modding.I1I0I.eventReceived = function(t) {
     var e, i, s, n, r;
