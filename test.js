1f1bee17e124a9152194116136e7f924f5db101a
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-05-08 15:28:57 +0700

    Update test.js

--- test.js
+++ test.js
@@ -152,16 +152,28 @@ Object.assign(game, GameExtender);
 Object.assign(I1l00.prototype, ShipExtender);
 Object.assign(Alien.prototype, AlienExtender);
 
+function checkTicks (game)  // all commands for tick goes here
+{
+    game.updateShips();
+    game.checkForTimers();
+    game.checkForIntervals()
+}
+
+function checkEvents (event, game)  // all commands for events handling goes here
+{
+    game.updateShips(event);
+}
+
 game.modding.tick = function(t) {
     var e;
-    if (this.game.tick(t), e = Date.now(),this.game.updateShips(), this.game.checkForTimers(), this.game.checkForIntervals(), null != this.context.tick && this.context.tick(this.game), e = Date.now() - e, this.max_tick_time = Math.max(this.max_tick_time, e), this.tick_time += e, this.tick_count += 1, this.tick_count >= 600) return this.terminal.echo("Tick CPU time: average " + Math.round(this.tick_time / this.tick_count) + " ms ; max " + Math.round(this.max_tick_time) + " ms"), this.terminal.echo("Data sent: " + Math.round(this.I1I0I.log_sent / this.tick_count * 60) + " bytes per second"), this.tick_count = 0, this.tick_time = 0, this.max_tick_time = 0, this.I1I0I.log_sent = 0
+    if (this.game.tick(t), e = Date.now(), checkTicks(this.game), null != this.context.tick && this.context.tick(this.game), e = Date.now() - e, this.max_tick_time = Math.max(this.max_tick_time, e), this.tick_time += e, this.tick_count += 1, this.tick_count >= 600) return this.terminal.echo("Tick CPU time: average " + Math.round(this.tick_time / this.tick_count) + " ms ; max " + Math.round(this.max_tick_time) + " ms"), this.terminal.echo("Data sent: " + Math.round(this.I1I0I.log_sent / this.tick_count * 60) + " bytes per second"), this.tick_count = 0, this.tick_time = 0, this.max_tick_time = 0, this.I1I0I.log_sent = 0
 }
 lOlO0.prototype.eventReceived = function(t) {
     var e, i, s, n, r;
     if (null != t.data) {
         null != t.data.ship && (r = this.modding.game.findShip(t.data.ship), t.data.ship = r), null != t.data.killer && (n = this.modding.game.findShip(t.data.killer), t.data.killer = n), null != t.data.alien && (e = this.modding.game.findAlien(t.data.alien), t.data.alien = e), null != t.data.asteroid && (i = this.modding.game.findAsteroid(t.data.asteroid), t.data.asteroid = i), null != t.data.collectible && (s = this.modding.game.findCollectible(t.data.collectible), t.data.collectible = s);
         try {
-            null != this.modding.context.event && (this.modding.game.updateShips(t.data), this.modding.context.event(t.data, this.modding.game))
+            null != this.modding.context.event && (checkEvents(t.data, this.modding.game), this.modding.context.event(t.data, this.modding.game))
         } catch (t) {
             t
         }
