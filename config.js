cb9fd2310496b48ca649cd9eb9fb32392763bedb
Takagumin <53442511+Bhpsngum@users.noreply.github.com>
2020-05-11 16:09:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,175 +1,87 @@
-/*Initial setup. Do not modify codes between the comments blocks! */
-const GameExtender = {
-    broadcastInterval:0,
-    timers: new Set(),
-    intervals: [],
-    print: function (item) {
-        this.modding.terminal.echo(item);
-    },
-    echo: function (item) {
-        this.print(item);
-    },
-    log: function (item) {
-        this.print(item);
-    },
-    error: function (item) {
-        this.modding.terminal.error(item);
-    },
-    kick: function (identifier) {
-        let ship = this.ships[identifier];
-        return ship && ship.gameover({
-            "Status": "Kicked by operator",
-            "Score": ship.score,
-            "High score": ship.highscore,
-            "Frags": ship.frag,
-            "Deaths": ship.death
-        });
-    },
-    kill: function (identifier) {
-        let ship = this.ships[identifier];
-        return ship && ship.set({
-            kill: true
-        });
-    },
-    locateShip: function (identifier) {
-        if (typeof identifier == "number") {
-            return this.ships[identifier];
+let success=1;
+try
+{
+    let fail=0;
+    $.ajax("https://raw.githubusercontent.com/bhpsngum/starblast-modding-plus/master/main.js")
+    .done(function (response) {
+        eval("function init(){"+response+"}");
+        
+        let parsed = new init();
+        
+        function checkTicks(game)
+        {
+            return parsed.tick && parsed.tick(game);
         }
-        let searchQuery = identifier.toLowerCase();
-        for (let shipIndex in this.ships) {
-            let ship = this.ships[shipIndex];
-            let shipName = ship.name.toLowerCase();
-            if (shipName.includes(searchQuery)) {
-                return ship;
-            }
+
+        function checkEvents(event, game)
+        {
+            return parsed.event && parsed.event(event, game);
         }
-        return null;
-    },
-    setTimeout: function (func, ticks) {
-        let currentTick = this.step;
-        this.timers.add([func, currentTick + ticks, this]);
-    },
-    setInterval: function (func, ticks) {
-        let currentTick = this.step;
-        return this.intervals.push([func, ticks]) - 1;
-    },
-    clearInterval: function (index) {
-        this.intervals.splice(index, 1);
-    },
-    checkForTimers: function () {
-        this.timers.forEach(function (timer) {
-            let game = timer[2];
-            if (game.step >= timer[1]) {
-                (typeof timer[0] == "function") && timer[0]();
-                game.timers.delete(timer);
-            }
-        });
-    },
-    checkForIntervals: function () {
-        this.intervals.forEach(function (interval) {
-            if (game.step % interval[1] === 0) {
-                (typeof interval[0] == "function") && interval[0]();
-            }
-        });
-    },
-    updateShips: function (event) {
-        if (!event)
-            this.ships.forEach(function (ship) {
-                ship.highscore = Math.max(ship.highscore || 0, ship.score);
-                ship.rotation = ship.r * 180 / Math.PI;
-            });
-        else
-            switch (event.name || "") {
-                case "ship_destroyed":
-                    if (!Object.is(event.killer, null)) event.killer.frag++;
-                    if (!Object.is(event.ship, null)) event.ship.death++;
+        
+        function showtick(req, isNotify, needEdit)
+        {
+            let suc=0,f;
+            switch((req||"").toLowerCase())
+            {
+                case "true":
+                case "false":
+                    f=req;
+                    suc=1;
                     break;
+                case "":
+                    if (["true","false"].indexOf(localStorage.showtick) == -1) f="true";
+                    suc=1;
+                    break;
+                default:
+                    if (needEdit)
+                        if (["true","false"].indexOf(localStorage.showtick) == -1) f="true";
+                    (isNotify) && game.modding.terminal.echo("Please specify true/false to proceed");
             }
-    },
-    instructorBroadcast: function(message, _instructor, _delay) {
-        _instructor = _instructor || "Lucina";
-        _delay = Number(_delay) || 120;
-        this.setTimeout(function() {
-          this.ships.forEach(function(ship) {
-            ship.instructorSays(message, _instructor);
-          });
-        }.bind(this), this.broadcastInterval);
-        this.broadcastInterval = this.broadcastInterval + _delay;
-        this.setTimeout(function() {
-            (this.broadcastInterval == _delay) && this.ships.forEach(function(ship) {
-                ship.hideInstructor();
-            });
-            this.broadcastInterval = this.broadcastInterval - _delay;
-        }.bind(this), this.broadcastInterval);
-    },
-    emptyWeapons: function () {
-      this.ships.forEach(function(ship) {
-        ship.emptyWeapons();
-      });
-    }
-};
-
-const ShipExtender = {
-  kill: function () {
-    this.set({
-      kill: true
-    });
-    return this;
-  },
-  frag: 0,
-  death: 0
-};
-
-for (let prop of ["invulnerable","angle"])
-  eval(`ShipExtender.${prop} = function(data) {
-    this.set({
-      ${prop}: data
-    });
-    return this;
-  }`);
-  
-const AlienExtender = {
-  kill: function () {
-    return this.set({
-      kill: true
-    });
-  },
-};
-
-Object.assign(Asteroid.prototype, AlienExtender);
-
-for (let prop of ["shield","regen","damage","laser_speed","rate"])
-  eval(`AlienExtender.${prop} = function(data) {
-    this.set({
-      ${prop}: data
-    });
-    return this;
-  }`);
-
-AlienExtender.laserSpeed = function (data) {
-    return this.laser_speed(data);
+            (f) && localStorage.setItem("showtick",f);
+            return suc;
+        }
+            
+        game.modding.commands.showtick = function(req)
+        {
+            let cmd=req.trim().split(" ");
+            
+            (showtick(cmd[1],1,1)) && game.modding.terminal.echo(`Automatic tick logging is ${(localStorage.showtick == "true")?"enabled":"disabled"}`);
+        }
+                    
+        game.modding.tick = function(t) {
+            var e;
+            if (this.game.tick(t), e = Date.now(), checkTicks(this.game), null != this.context.tick && this.context.tick(this.game), e = Date.now() - e, this.max_tick_time = Math.max(this.max_tick_time, e), this.tick_time += e, this.tick_count += 1, this.tick_count >= 600) return showtick(null,0),(localStorage.showtick == "true")?(this.terminal.echo("Tick CPU time: average " + Math.round(this.tick_time / this.tick_count) + " ms ; max " + Math.round(this.max_tick_time) + " ms"), this.terminal.echo("Data sent: " + Math.round(this.I1I0I.log_sent / this.tick_count * 60) + " bytes per second")):void 0, this.tick_count = 0, this.tick_time = 0, this.max_tick_time = 0, this.I1I0I.log_sent = 0
+        }
+        lOlO0.prototype.eventReceived = function(t) {
+            var e, i, s, n, r;
+            if (null != t.data) {
+                null != t.data.ship && (r = this.modding.game.findShip(t.data.ship), t.data.ship = r), null != t.data.killer && (n = this.modding.game.findShip(t.data.killer), t.data.killer = n), null != t.data.alien && (e = this.modding.game.findAlien(t.data.alien), t.data.alien = e), null != t.data.asteroid && (i = this.modding.game.findAsteroid(t.data.asteroid), t.data.asteroid = i), null != t.data.collectible && (s = this.modding.game.findCollectible(t.data.collectible), t.data.collectible = s);
+                try {
+                    checkEvents(t.data, this.modding.game), null != this.modding.context.event && this.modding.context.event(t.data, this.modding.game)
+                } catch (t) {
+                    t
+                }
+                switch (t.data.name) {
+                    case "asteroid_destroyed":
+                        if (null != i) return i.killed = !0;
+                        break;
+                    case "alien_destroyed":
+                        if (null != e) return e.killed = !0;
+                        break;
+                    case "collectible_picked":
+                        if (null != s) return s.killed = !0
+                }
+            }
+        }
+    })
+    .fail(() => {fail=1;});
+    
+    if (fail) throw "Fetching failed";
 }
-  
-Object.assign(game, GameExtender);
-Object.assign(I1l00.prototype, ShipExtender);
-Object.assign(Alien.prototype, AlienExtender);
-
-/* End of initial setup */
-
-this.options = {
-    // see documentation for options reference
-    root_mode: "survival",
-    map_size: 30
-};
-
-this.tick = function (game) {
-    game.updateShips();
-    game.checkForTimers();
-    game.checkForIntervals();
-    // do mod stuff here ; see documentation
-};
-
-this.event = function (event, game) {
-    game.updateShips(event);
-    // Place your event handler code here
+catch(e)
+{
+    success=0;
+    console.log(e);
+    game.modding.terminal.error("An error occured while executing the Modding+ extensions!");
 }
+(success) && game.modding.terminal.echo("Successfully initialized Modding+ extensions into the Modding engine!");
