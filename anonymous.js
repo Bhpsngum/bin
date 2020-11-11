d98f3981d4b6f35159724a4138d08ed70cd9881f
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-11-11 09:31:49 +0700

    hmm

--- anonymous.js
+++ anonymous.js
@@ -2,6 +2,10 @@ try {
   let u = module.exports, t = u.settings, data, mode, anonymous_hue = 60,
   setHue = function(prop) {
     prop.hue = anonymous_hue;
+  }, getVal = function(obj, dirarr) {
+    let t = obj;
+    for (let i of dirarr) t = t[i];
+    return t;
   }
   for (let i in t)
     if (t[i].settings) {
@@ -16,23 +20,14 @@ try {
     setHue(mode.options.teams[i]);
   }
   let tx = data.names.getData.bind(data.names), ty = Pods.prototype.getModelInstance, tz = Mine.prototype.read, tw = Rocket.prototype.read, rev, add, stat;
-  if (window.location.pathname == "/app.html") {
-    rev = "l0OO1";
-    add = data.I1l11.IO000;
-    stat = "ll0l1";
-  }
-  else {
-    rev = "OIOOI";
-    add = data.I110O.I11I0;
-    stat = "l1OlO"
-  }
   data.names.getData = function(u){
     let x = tx(u);
     x && setHue(x);
     return x
   }
+  ty.toString().replace(/this\.(((?!(this)).)+?)\.hue/, function(...v){stat=v[1].split(".")});
   Pods.prototype.getModelInstance = function(t) {
-    setHue(this[stat].status);
+    setHue(getVal(this, stat));
     return ty.bind(this)(t);
   }
   Mine.prototype.read = function(t) {
@@ -45,11 +40,30 @@ try {
     setHue(this);
     return x;
   }
-  for (let team of mode.teams) team.station_model[rev].remove(...team.station_model.modules.map(i => i[rev]))
-  add.stations = [];
-  for (let team of mode.teams) {
-    team.station_model = new StationModel(team.station_desc, team);
-    add.addStation(team.station_model)
+  for (let i in mode.team.station_model) {
+    if (mode.team.station_model[i].type == "Group") {
+      rev = i;
+      break;
+    }
+  }
+  for (let i in data) {
+    if (data[i].seed) {
+      for (let j in data[i]) {
+        if (Array.isArray(data[i][j].stations)) {
+          add = data[i][j];
+          break;
+        }
+      }
+      break;
+    }
+  }
+  if (mode.id == "team") {
+    for (let team of mode.teams) team.station_model[rev].remove(...team.station_model.modules.map(i => i[rev]))
+    add.stations = [];
+    for (let team of mode.teams) {
+      team.station_model = new StationModel(team.station_desc, team);
+      add.addStation(team.station_model);
+    }
   }
 }
 catch(e){console.log(e)};
